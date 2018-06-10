4b3457a70aabfec22070a46d395706c716af6e4c
Gustavo RPS <email+gitkraken.com@gustavorps.net>
2018-06-10 18:09:25 -0300

    removing some lines and making the variable code more explicit

--- src/haemapy/banner/model.py
+++ src/haemapy/banner/model.py
@@ -21,13 +21,22 @@ NSMAP = {
 class BannerBloodDonation(object):
 
   templates_directory = os.path.join(FILE_PATH, '../../../assets')
+  el_selectors = {
+    'recipient_image': '//svg:image[@id="recipient_image"]',
+    'recipient_name_part_1': '//svg:tspan[@id="recipient_name_part_1"]',
+    'recipient_name_part_2': '//svg:tspan[@id="recipient_name_part_2"]',
+    'recipient_blood_type': '//svg:tspan[@id="recipient_blood_type"]',
+    'location_name': '//svg:tspan[@id="location_name"]',
+    'location_address_part_1': '//svg:tspan[@id="location_address_part_1"]',
+    'location_address_part_2': '//svg:tspan[@id="location_address_part_2"]',
+  }
 
   def _make_template_path(self, filename, extension='svg'):
     return os.path.join(self.templates_directory,
                         '{}.{}'.format(filename, extension),)
 
-  def _validate_donate_action(self, donate_action):
-    assert isinstance(donate_action, dict)
+  def _validate_donate_action(self, data):
+    assert isinstance(data, dict)
     return True
 
   def __init__(self, 
@@ -35,60 +44,54 @@ class BannerBloodDonation(object):
                **kargs):
     template_path = self._make_template_path(template_name)
     assert os.path.exists(template_path)
-    donate_action = kargs['donate_action']
-    assert self._validate_donate_action(donate_action)
+    data = kargs['donate_action']
+    assert self._validate_donate_action(data)
     
     self.template_path = template_path
-    self.donate_action = donate_action 
+    self.data = data 
 
 
   def save(self, fp, format='png', **params):
-    image = self.donate_action['recipient_image']
-    name = self.donate_action['recipient_name']
-    blood_type = self.donate_action['recipient_blood_type']
-    place_name = self.donate_action['location_name']
-    address_street = self.donate_action['location_address_street']
-    address_number = self.donate_action['location_address_number']
-    address_district = self.donate_action['location_address_district']
-    address_locality = self.donate_action['location_address_locality']
-    address_region = self.donate_action['location_address_region']
-    address_postal_code = self.donate_action['location_address_postal_code']
+    d = self.data
+    recipient_image = self.data['recipient_image']
 
     tree = etree.parse(self.template_path)
 
-    image_mime_type = mimetypes.guess_type(image.name)
-    image_base_64 = base64.b64encode(image.read()).decode('utf-8')
-    e_image = tree.xpath('//svg:image[@id="image"]',namespaces=NSMAP)[0]
+    image_mime_type = mimetypes.guess_type(recipient_image.name)
+    image_base_64 = base64.b64encode(recipient_image.read()).decode('utf-8')
+    el_image = tree.xpath(self.el_selectors['recipient_image'], namespaces=NSMAP)[0]
+    el_image.attrib['{http://www.w3.org/1999/xlink}href'] = 'data:{};base64,{}'.format(image_mime_type[0], 
+                                                                                       image_base_64)
     
-    e_image.attrib['{http://www.w3.org/1999/xlink}href'] = 'data:{};base64,{}'.format(image_mime_type[0], image_base_64)
+    recipient_name_parts = d['recipient_name'].split()
     
-    name_parts = name.split()
-    
-    if len(name_parts) > 3:
-      name_l1 = ' '.join(name_parts[:3])
-      name_l2 = ' '.join(name_parts[3:])
+    if len(recipient_name_parts) > 3:
+      recipient_name_part_1 = ' '.join(recipient_name_parts[:3])
+      recipient_name_part_2 = ' '.join(recipient_name_parts[3:])
     else:
-      name_l1 = name
-      name_l2 = ''
+      recipient_name_part_1, recipient_name_part_2 = name, ''
     
-    e_name_l1 = tree.xpath('//svg:tspan[@id="name_l1"]',namespaces=NSMAP)[0]
-    e_name_l1.text = name_l1.upper()
-    e_name_l2 = tree.xpath('//svg:tspan[@id="name_l2"]',namespaces=NSMAP)[0]
-    e_name_l2.text = name_l2.upper()
+    el_r_name_part_1 = tree.xpath(self.el_selectors['recipient_name_part_1'], namespaces=NSMAP)[0]
+    el_r_name_part_1.text = recipient_name_part_1.upper()
     
-    e_blood_type = tree.xpath('//svg:tspan[@id="blood_type"]',namespaces=NSMAP)[0]
-    e_blood_type.text = blood_type.upper()
-    e_place_name = tree.xpath('//svg:tspan[@id="place_name"]',namespaces=NSMAP)[0]
-    e_place_name.text = place_name.upper()
+    el_r_name_part_2 = tree.xpath(self.el_selectors['recipient_name_part_2'], namespaces=NSMAP)[0]
+    el_r_name_part_2.text = recipient_name_part_2.upper()
     
-    e_address_l1 = tree.xpath('//svg:tspan[@id="address_l1"]',namespaces=NSMAP)[0]
-    e_address_l1.text = '{}, {}'.format(address_street.upper(), 
-                                        address_number.upper())
-    e_address_l2 = tree.xpath('//svg:tspan[@id="address_l2"]',namespaces=NSMAP)[0]
-    e_address_l2.text = '{}, {} - {}, {}'.format(address_district.upper(), 
-                                             address_locality.upper(),
-                                             address_region.upper(),
-                                             address_postal_code.upper())
+    el_r_blood_type = tree.xpath(self.el_selectors['recipient_image'], namespaces=NSMAP)[0]
+    el_r_blood_type.text = self.data['recipient_blood_type'].upper()
+    
+
+    el_l_name = tree.xpath(self.el_selectors['location_name'], namespaces=NSMAP)[0]
+    el_l_name.text = self.data['location_name'].upper()
+    
+    el_l_address_part_1= tree.xpath(self.el_selectors['location_address_part_1'],namespaces=NSMAP)[0]
+    el_l_address_part_1.text = '{}, {}'.format(self.data['location_address_street'].upper(), 
+                                               self.data['location_address_number'].upper())
+    el_l_address_part_2 = tree.xpath(self.el_selectors['location_address_part_2'],namespaces=NSMAP)[0]
+    el_l_address_part_2.text = '{}, {} - {}, {}'.format(self.data['location_address_district'].upper(), 
+                                                        self.data['location_address_locality'].upper(),
+                                                        self.data['location_address_region'].upper(),
+                                                        self.data['location_address_postal_code'].upper(),)
     
     format = format.lower()
 
