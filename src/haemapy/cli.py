677ac4e738a4fc03a6773037462840348af8ea84
Gustavo RPS <email+gitkraken.com@gustavorps.net>
2018-06-10 19:52:34 -0300

    init implemententation of media_objects endpoint with apistar

--- src/haemapy/cli.py
+++ src/haemapy/cli.py
@@ -1,8 +1,9 @@
 import click
 
 from .banner.cli import cli_root as banner_cli
+from .server import cli as server_cli
 
-haemapy_cli = click.CommandCollection(sources=[banner_cli])
+haemapy_cli = click.CommandCollection(sources=[banner_cli, server_cli])
 
 if __name__ == '__main__':
     haemapy_cli()
