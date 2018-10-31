8565bafd1bc37161251142f70d91223cc2b9356a
Mikael Hadler <mikaelhadler@gmail.com>
2018-10-31 18:45:58 -0300

    Resolve google-place-auto-complete;

--- site/components/hematopy-client-vuejs/src/components/Maps.vue
+++ site/components/hematopy-client-vuejs/src/components/Maps.vue
@@ -1,11 +1,38 @@
 <template>
-  <div></div>
+  <div>
+    <input ref="autocomplete" 
+    placeholder="Informe o endereço do hospital" 
+    class="search-location"
+    onfocus="value = ''" 
+    type="text" />
+  </div>
 </template>
 <script>
 export default {
   name: 'Maps',
-  mounted () {
-    console.log('here')
+  created() {
+    this.setGoogleApiPlace()
+  },
+  mounted() {
+    setTimeout(() => {
+      this.setConfigGoogleApi()
+    }, 500)
+  },
+  methods: {
+    setGoogleApiPlace () {
+      return new Promise(resolve => {
+        let recaptchaScript = document.createElement('script')
+        recaptchaScript.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDnOXTF1Mzm7OJWbWlP4AdhNvEfLCiX6-w&libraries=places')
+        document.head.appendChild(recaptchaScript)
+        resolve(true)
+      });
+    },
+    setConfigGoogleApi () {
+      this.autocomplete = new google.maps.places.Autocomplete(
+        (this.$refs.autocomplete),
+        {types: ['hospital']}
+      );
+    }
   }
 }
 </script>
